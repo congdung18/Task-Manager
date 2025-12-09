ac33734b94eb8758ea3963cfaa2fb290d156a15f
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-12-09 19:40:45 +0700

    refactor: environemt

--- api/db/connect.js
+++ api/db/connect.js
@@ -1,4 +1,8 @@
-require('dotenv').config()
+const path = require('path')
+
+require('dotenv').config({
+    path: path.resolve(__dirname, '../environment/.env')
+})
 const mongoose = require('mongoose')
 
 const connectDB = async () => {
