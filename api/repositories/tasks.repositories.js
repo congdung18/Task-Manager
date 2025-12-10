9c6cd0e55e4150315df2d7fb2bba3a59021481eb
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-12-10 09:57:04 +0700

    refactor: tasks repository

---
+++ api/repositories/tasks.repositories.js
@@ -0,0 +1,36 @@
+const Task = require('../models/tasks.model.js')
+
+class TaskRepositories{
+    getAllTasks(filter, sort, skip, limit){
+        return Task.find(filter).sort(sort).skip(skip).limit(limit)
+    }
+
+    getTotal(filter){
+        return Task.countDocuments(filter)
+    }
+
+    createTask(body){
+        return Task.create(body)
+    }
+
+    getTask(filter){
+        return Task.findOne(filter)
+    }
+
+    updateTask(filter, update){
+        return Task.findOneAndUpdate(filter, update, {
+            new: true,
+            runValidators: true
+        })
+    }
+
+    deleteTask(filter){
+        return Task.findOneAndDelete(filter)
+    }
+
+    deleteAllTasks(filter){
+        return Task.deleteMany(filter)
+    }
+}
+
+module.exports = new TaskRepositories()
