536d8b4531876dbfe5773c1541661091e9fcc7a2
theelphenomeno10 <theelphenomeno10@gmail.com>
2025-12-13 17:31:57 +0700

    refactor: migrate app and server to TypeScript

---
+++ api/app.ts
@@ -0,0 +1,30 @@
+import express, {Application} from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import tasks from './routes/tasks.route';
+import admin from './routes/admin.route';
+import auth from './routes/authentication.route';
+
+import errorHandling from './middlewares/errors/error_handling';
+import morganMiddleware from './middlewares/logging/morgan';
+
+const app: Application = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:5173']
+}));
+
+app.use(morganMiddleware);
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1', auth);
+app.use('/api/v1/tasks', tasks);
+app.use('/api/v1/admin', admin);
+
+app.use(errorHandling);
+
+export default app
