9339dae84ab85d81ad6917088fc1ebf92d91b974
theelphenomeno10 <theelphenomeno10@gmail.com>
2026-01-06 10:17:01 +0700

    refactor: ts

--- api/utils/generate_token.ts
+++ api/utils/generate_token.ts
@@ -1,35 +1,20 @@
-import jwt, {SignOptions} from "jsonwebtoken"
-
-interface UserPayload{
-    _id: String,
-    username: String,
-    role: String
-}
+import jwt, { SignOptions } from "jsonwebtoken"
+import { UserPayload } from '../interfaces/auth/user_payload'
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string
 
 export const generateAccessToken = (user: UserPayload): string => {
-    const payload = {
-        id: user._id,
-        username: user.username,
-        role: user.role
-    }
-
     const options: SignOptions = {
         expiresIn: "15m"
     }
 
-    return jwt.sign(payload, JWT_SECRET_KEY, options)
+    return jwt.sign(user, JWT_SECRET_KEY, options)
 }
 
-export const generateRefreshToken = (user: Pick<UserPayload, "_id">): string => {
-    const payload = {
-        id: user._id
-    }
-
+export const generateRefreshToken = (user: Pick<UserPayload, "id">): string => {
     const options: SignOptions = {
         expiresIn: "7d"
     }
 
-    return jwt.sign(payload, JWT_SECRET_KEY, options)
+    return jwt.sign(user, JWT_SECRET_KEY, options)
 }
