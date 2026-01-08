0c8e66255416e49614acf5242df635dbdcc3461b
theelphenomeno10 <theelphenomeno10@gmail.com>
2026-01-08 15:39:56 +0700

    add: status and priority type

--- api/interfaces/model/task.interface.ts
+++ api/interfaces/model/task.interface.ts
@@ -1,5 +1,19 @@
 import { Document } from 'mongoose'
 
+export enum TaskStatus {
+    PENDING = "pending",
+    IN_PROGRESS = "in-progress",
+    DONE = "done",
+    OVERDUE = "overdue",
+}
+
+export enum TaskPriority {
+    EMERGENCY = "emergency",
+    HIGH = "high",
+    MEDIUM = "medium",
+    LOW = "low",
+}
+
 export interface ITask extends Document{
     name: string
     expiry_date: Date
