44d0690bdc203fa3402a6aca67d8b5171b97007f
theelphenomeno10 <theelphenomeno10@gmail.com>
2026-01-22 23:14:18 +0700

    add: status and priority

--- api/validators/query.ts
+++ api/validators/query.ts
@@ -1,6 +1,11 @@
 import { query, ValidationChain } from 'express-validator';
 
+const VALID_STATUS_MESSAGE = ["pending", "in-progress", "done", "overdue"];
+const VALID_PRIORITY_MESSAGE = ["emergency", "high", "medium", "low"];
+
 export const validationPagination: ValidationChain[] = [
     query("page").optional().isInt({ min: 1 }),
-    query("limit").optional().isInt({ min: 1, max: 100 })
+    query("limit").optional().isInt({ min: 1, max: 100 }),
+    query("status").optional().isIn(VALID_STATUS_MESSAGE),
+    query("priority").optional().isIn(VALID_PRIORITY_MESSAGE)
 ];
